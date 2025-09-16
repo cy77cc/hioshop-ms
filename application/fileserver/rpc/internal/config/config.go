7271ff1bd636d33abd24e2d6280a49c7eefe9720
zhangdongping <zhangdongping@ut.cn>
2025-09-16 10:31:48 +0800

    feat: 实现文件服务上传的rpc逻辑，需完善

--- application/fileserver/rpc/internal/config/config.go
+++ application/fileserver/rpc/internal/config/config.go
@@ -1,7 +1,23 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	Minio MinioConfig
+	Mysql struct {
+		DataSource string
+	}
+	cache.CacheConf
+}
+
+type MinioConfig struct {
+	AccessKeyID     string
+	Endpoint        string
+	SecretAccessKey string
+	UseSSL          bool
+	Bucket          string
 }
