f16005801a21d1c776b29cb17b7529ab4747ead1
cy77cc <483564813@qq.com>
2025-09-21 23:01:53 +0800

    upd(fileserver-rpc): 实现文件上传rpc接口

---
+++ app/fileserver/rpc/internal/logic/uploadlogic.go
@@ -0,0 +1,101 @@
+package logic
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/cy77cc/hioshop_ms/app/fileserver/rpc/internal/svc"
+	"github.com/cy77cc/hioshop_ms/app/fileserver/rpc/pb"
+	"github.com/minio/minio-go/v7"
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UploadLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewUploadLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UploadLogic {
+	return &UploadLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 流式上传分片
+func (l *UploadLogic) Upload(in *pb.UploadReq) (*pb.UploadResp, error) {
+	object_name := getObjectName(in.Hash)
+
+	// 0 表示是开始请求创建分片上传，在这里面应该判断
+	if in.PartNumber == 0 {
+		// 判断这个文件对象是否存在
+		stat, err := l.svcCtx.MinioClient.StatObject(l.ctx, in.Bucket, object_name, minio.StatObjectOptions{})
+		resp := minio.ToErrorResponse(err)
+		if err != nil {
+			// 有报错，而且错误不是对象不存在
+			if resp.Code != minio.NoSuchKey {
+				return nil, err
+			}
+		}
+
+		if resp.Code == minio.NoSuchKey {
+			uploadID, err := l.svcCtx.MinioCore.NewMultipartUpload(l.ctx, in.Bucket, object_name, minio.PutObjectOptions{})
+			if err != nil {
+				return nil, err
+			}
+			return &pb.UploadResp{
+				UploadId: uploadID,
+				Etag:     "",
+			}, nil
+		}
+		// 表示已经存在这个文件对象了
+		return &pb.UploadResp{
+			UploadId: "",
+			Etag:     stat.ETag,
+		}, nil
+	}
+
+	// 不是第一上传
+	reader := bytes.NewReader(in.Data)
+	part, err := l.svcCtx.MinioCore.PutObjectPart(l.ctx, in.Bucket, object_name, in.UploadId, int(in.PartNumber), reader, in.FileSize, minio.PutObjectPartOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	if in.IsLast {
+		listObjectParts, err := l.svcCtx.MinioCore.ListObjectParts(l.ctx, in.Bucket, object_name, in.UploadId, 0, int(in.PartNumber))
+		if err != nil {
+			return nil, err
+		}
+
+		completePart := make([]minio.CompletePart, len(listObjectParts.ObjectParts))
+
+		for i, v := range listObjectParts.ObjectParts {
+			completePart[i].PartNumber = v.PartNumber
+			completePart[i].ETag = v.ETag
+			completePart[i].ChecksumCRC32 = v.ChecksumCRC32
+			completePart[i].ChecksumSHA256 = v.ChecksumSHA256
+		}
+
+		completeMultipartUpload, err := l.svcCtx.MinioCore.CompleteMultipartUpload(l.ctx, in.Bucket, object_name, in.UploadId, completePart, minio.PutObjectOptions{})
+		if err != nil {
+			return nil, err
+		}
+
+		return &pb.UploadResp{
+			UploadId: in.UploadId,
+			Etag:     completeMultipartUpload.ETag,
+		}, nil
+	}
+
+	return &pb.UploadResp{
+		UploadId: in.UploadId,
+		Etag:     part.ETag,
+	}, nil
+}
+
+func getObjectName(hash string) string {
+	return fmt.Sprintf("%s/%s", string(hash[:5]), hash)
+}
